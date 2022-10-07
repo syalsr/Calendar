ddaac1bdfd171e25dbe4c9cc09e1ec7fccb98f8f
volobuevrostislav <volobuevrostislav@gmail.com>
2022-10-07 13:12:16 +0300

    Handle funcs EventPer...

--- interntal/api/calendar.go
+++ interntal/api/calendar.go
@@ -36,33 +36,37 @@ func (c *calendar) DeleteEvent(UUID int) {
 
 	delete(c.Events, UUID)
 }
-func (c *calendar) GetEventPerDay(day time.Time) []model.Event {
+func (c *calendar) GetEventPerDay(day time.Time, userID int) []model.Event {
 	var events []model.Event
 
 	for _, event := range c.Events {
-		if event.Date == day {
+		if event.Date == day && event.UserID == userID {
 			events = append(events, event)
 		}
 	}
 	return events
 }
 
-func (c *calendar) GetEventPerWeek(day time.Time) []model.Event {
+func (c *calendar) GetEventPerWeek(day time.Time, userID int) []model.Event {
 	var events []model.Event
 
 	for _, event := range c.Events {
-		if event.Date.After(day) && event.Date.Before(day.AddDate(0, 0, 7)) {
+		if event.Date.After(day) &&
+			event.Date.Before(day.AddDate(0, 0, 7)) &&
+			event.UserID == userID {
 			events = append(events, event)
 		}
 	}
 	return events
 }
 
-func (c *calendar) GetEventPerMonth(day time.Time) []model.Event {
+func (c *calendar) GetEventPerMonth(day time.Time, userID int) []model.Event {
 	var events []model.Event
 
 	for _, event := range c.Events {
-		if event.Date.After(day) && event.Date.Before(day.AddDate(0, 1, 0)) {
+		if event.Date.After(day) &&
+			event.Date.Before(day.AddDate(0, 1, 0)) &&
+			event.UserID == userID {
 			events = append(events, event)
 		}
 	}
--- interntal/handler/handler.go
+++ interntal/handler/handler.go
@@ -11,6 +11,8 @@ import (
 	"time"
 )
 
+const layout = "2006-01-02"
+
 func InitHandlers() {
 	http.HandleFunc("/create_event", CreateEvent)
 	http.HandleFunc("/update_event", UpdateEvent)
@@ -22,6 +24,54 @@ func InitHandlers() {
 
 func CreateEvent(writter http.ResponseWriter, request *http.Request) {
 	if request.Method == http.MethodGet {
+		writter.WriteHeader(http.StatusMethodNotAllowed)
+		writter.Write([]byte("{error: bad request}"))
+		return
+	}
+
+	var event model.Event
+
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		writter.Write([]byte(`{"error": "bad request - incorrect body"}`))
+		return
+	}
+
+	js := make(map[string]string)
+	err = json.Unmarshal(body, &js)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	event.Event = js["event"]
+
+	event.Date, err = time.Parse(layout, js["date"])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	event.UserID, err = strconv.Atoi(js["user_id"])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	event.UUID = api.Calendar.LastUUID + 1
+	api.Calendar.LastUUID = event.UUID
+
+	api.Calendar.CreateEvent(&event)
+
+	writter.WriteHeader(http.StatusOK)
+	writter.Write([]byte("{result: success"))
+
+	log.Println(event)
+}
+
+func UpdateEvent(writter http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		writter.WriteHeader(http.StatusMethodNotAllowed)
 		writter.Write([]byte("{error: bad request}"))
 		return
 	}
@@ -34,36 +84,159 @@ func CreateEvent(writter http.ResponseWriter, request *http.Request) {
 	}
 
 	js := make(map[string]string)
-	json.Unmarshal(body, &js)
+	err = json.Unmarshal(body, &js)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	event.Event = js["event"]
-	event.Date, _ = time.Parse("2000-10-05", js["date"])
-	event.UserID, _ = strconv.Atoi(js["user_id"])
-	event.UUID = api.Calendar.LastUUID + 1
-	api.Calendar.LastUUID = event.UUID
 
-	api.Calendar.CreateEvent(&event)
+	event.Date, err = time.Parse(layout, js["date"])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	event.UserID, err = strconv.Atoi(js["user_id"])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	event.UUID, err = strconv.Atoi(js["UUID"])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	api.Calendar.UpdateEvent(&event)
+
+	writter.WriteHeader(http.StatusOK)
 	writter.Write([]byte("{result: success"))
 
 	log.Println(event)
 }
 
-func UpdateEvent(writter http.ResponseWriter, request *http.Request) {
-
-}
-
 func DeleteEvent(writter http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		writter.WriteHeader(http.StatusMethodNotAllowed)
+		writter.Write([]byte("{error: bad request}"))
+		return
+	}
+	var event model.Event
+	body, err := io.ReadAll(request.Body)
+	if err != nil {
+		writter.WriteHeader(http.StatusBadRequest)
+		writter.Write([]byte(`{"error": "bad request - incorrect body"}`))
+		return
+	}
 
+	js := make(map[string]string)
+	err = json.Unmarshal(body, &js)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	event.UUID, err = strconv.Atoi(js["UUID"])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	api.Calendar.DeleteEvent(event.UUID)
+
+	writter.WriteHeader(http.StatusOK)
+	writter.Write([]byte("{result: success"))
 }
 
 func EventsForDay(writter http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodPost {
+		writter.WriteHeader(http.StatusMethodNotAllowed)
+		writter.Write([]byte("{error: bad request}"))
+		return
+	}
 
+	userID, err := strconv.Atoi(request.URL.Query().Get("user_id"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	date, err := time.Parse(layout, request.URL.Query().Get("date"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	events := api.Calendar.GetEventPerDay(date, userID)
+	for _, event := range events {
+		js, err := json.MarshalIndent(event, "", "\t")
+		if err != nil {
+			log.Println(err)
+		}
+		writter.Write(js)
+	}
+	writter.WriteHeader(http.StatusOK)
 }
 
 func EventsForWeek(writter http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodPost {
+		writter.WriteHeader(http.StatusMethodNotAllowed)
+		writter.Write([]byte("{error: bad request}"))
+		return
+	}
 
+	userID, err := strconv.Atoi(request.URL.Query().Get("user_id"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	date, err := time.Parse(layout, request.URL.Query().Get("date"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	events := api.Calendar.GetEventPerDay(date, userID)
+	for _, event := range events {
+		js, err := json.MarshalIndent(event, "", "\t")
+		if err != nil {
+			log.Println(err)
+		}
+		writter.Write(js)
+	}
+	writter.WriteHeader(http.StatusOK)
 }
 
 func EventsForMonth(writter http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodPost {
+		writter.WriteHeader(http.StatusMethodNotAllowed)
+		writter.Write([]byte("{error: bad request}"))
+		return
+	}
 
+	userID, err := strconv.Atoi(request.URL.Query().Get("user_id"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	date, err := time.Parse(layout, request.URL.Query().Get("date"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	events := api.Calendar.GetEventPerDay(date, userID)
+	for _, event := range events {
+		js, err := json.MarshalIndent(event, "", "\t")
+		if err != nil {
+			log.Println(err)
+		}
+		writter.Write(js)
+	}
+	writter.WriteHeader(http.StatusOK)
 }
